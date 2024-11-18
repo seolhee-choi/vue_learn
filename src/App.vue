456032a4358fb2405eb39fc05823c25d92d7840b
seolhee-choi <csheeee38@gmail.com>
2024-11-18 17:50:55 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
+  <h1 v-html="rawHtml2"></h1>
+
+  <h2 :class="{active: isActive}">클래스 바인딩 테스트</h2>
+  <button @click="change">버튼</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rawHtml: "이것은 텍스트입니다.",
+      rawHtml2: "<span style='color:red'>이것은 빨간색이어야 합니다.</span>",
+      isActive: false,
+    }
+  },
+  methods: {
+    change() {
+      this.isActive = !this.isActive
+    },
+  }
+}
+</script>
+
+<style scoped>
+h2.active{
+  color: green;
+}
+
+</style>
